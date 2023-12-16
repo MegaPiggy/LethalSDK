9878fc57a977d8ebbe3bd447b3f61a31b1902e20
DENIEL-VALERO Ewan <ewandenielvalero@orange.fr>
2023-12-16 02:04:15 +0100

    Update OldAssetsRemover.cs

--- LethalSDK/Editor/OldAssetsRemover.cs
+++ LethalSDK/Editor/OldAssetsRemover.cs
@@ -23,6 +23,19 @@ namespace LethalSDK.Editor
             "Assets/Mods/TemplateMod/Moons/OldSeaPort.asset",
             "Assets/Mods/TemplateMod/Moons/Sky and Fog Global Volume Profile.asset",
             "Assets/Mods/TemplateMod/Moons/Sky and Fog Global Volume Profile 1.asset",
+            "Assets/Mods/TemplateMod/AssetBank.asset",
+            "Assets/Mods/LethalExpansion/Animations/Sun/TimeOfDaySunCompanyLevel.anim",
+            "Assets/Mods/LethalExpansion/Animations/Sun/TimeOfDaySunTypeB.anim",
+            "Assets/Mods/LethalExpansion/Animations/Sun/TimeOfDaySunTypeBEclipse.anim",
+            "Assets/Mods/LethalExpansion/Animations/Sun/TimeOfDaySunTypeBStormy.anim",
+            "Assets/Mods/LethalExpansion/Animations/Sun/TimeOfDaySunTypeC.anim",
+            "Assets/Mods/LethalExpansion/Animations/Sun/TimeOfDaySunTypeCEclipse.anim",
+            "Assets/Mods/LethalExpansion/Animations/Sun/TimeOfDaySunTypeCStormy.anim",
+            "Assets/Mods/LethalExpansion/Skybox",
+            "Assets/Mods/LethalExpansion/Sprites/XButton.png",
+            "Assets/Mods/LethalExpansion/Textures/sunTexture1.png",
+            "Assets/Mods/OldSeaPort/Materials/Maple_bark_1.mat",
+            "Assets/Mods/OldSeaPort/Materials/maple_leaves.mat",
             "Assets/Mods/TemplateMod/AssetBank.asset"
         };
 
