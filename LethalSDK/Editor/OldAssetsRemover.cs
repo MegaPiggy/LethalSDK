50a0d20291012f3db3ac2b2bf244d5c28c002a27
DENIEL-VALERO Ewan <ewandenielvalero@orange.fr>
2023-12-22 18:00:30 +0100

    1.3.0

--- LethalSDK/Editor/OldAssetsRemover.cs
+++ LethalSDK/Editor/OldAssetsRemover.cs
@@ -40,6 +40,7 @@ namespace LethalSDK.Editor
             "Assets/Mods/OldSeaPort/EffectExamples/Shared/Scripts",
             "Assets/Mods/OldSeaPort/scenes",
             "Assets/Mods/OldSeaPort/prefabs/Plane (12).prefab",
+            "Assets/Mods/LethalExpansion/Meshes/labyrinth.fbx"
         };
 
         [InitializeOnLoadMethod]
